4b5405114d6f88dabef8689fc9f971f84fb54a4b
luis-keny <lucerobalvinluiskeny@gmail.com>
2024-11-09 09:05:57 -0500

    chore: filter products pipe

---
+++ src/app/core/pipe/filter-product.pipe.ts
@@ -0,0 +1,35 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+interface Product {
+  id:          number;
+  title:       string;
+  price:       number;
+  description: string;
+  category:    Category;
+  images:      string[];
+}
+
+interface Category {
+  id:    number;
+  name:  string;
+  image: string;
+}
+
+@Pipe({
+  name: 'filterProduct'
+})
+export class FilterProductPipe implements PipeTransform {
+
+  transform(products: Product[], searchText: string): Product[] {
+    if(products.length == 0) return products;
+    if(searchText == '') return products;
+
+    return products.filter(product => product.title.includes(searchText)) ||
+      products.filter(product => product.description.includes(searchText)) ||
+      products.filter(product => product.price.toString().includes(searchText)) ||
+      products.filter(product => product.description.includes(searchText)) ||
+      products.filter(product => product.category.name.includes(searchText))
+    ;
+  }
+
+}
--- src/app/layout/home/home.module.ts
+++ src/app/layout/home/home.module.ts
@@ -7,11 +7,13 @@ import { HeaderComponent } from '../../shared/components/header/header.component
 import { CardComponent } from '../../shared/components/card/card.component';
 import { FloatLabelComponent } from '../../shared/components/float-label/float-label.component';
 import { SearchComponent } from '../../shared/components/search/search.component';
+import { FilterProductPipe } from '../../core/pipe/filter-product.pipe';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    FilterProductPipe,
   ],
   imports: [
     CommonModule,
